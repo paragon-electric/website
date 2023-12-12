a59e85dd46e41751ac6bb570bb42ce1115c833b0
Kirk Davies <kdavies@marchnetworks.com>
2023-12-12 08:11:08 -0500

    Auto-generated commit [ci skip]

--- src/app/constants.ts
+++
@@ -1,6 +0,0 @@
-export const APP_NAME = 'Paragon Electric';
-export const FULL_COMPANY_NAME = 'Paragon Electric Ontario Inc.';
-
-export const PHONE_NUMBER = '+1 (613) 265-3330';
-
-export const EMAIL = 'ParagonElectricOntario@gmail.com';
